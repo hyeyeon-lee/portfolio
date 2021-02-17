3cad1a81813215fba556f7662f3ce848d789d3e3
hyeyeon-lee <dear.yeonee@gmail.com>
2021-02-17 21:28:53 +0900

    feat: nav media query 적용
    1. font-awesome src 변경
    2. script로 class 제어 (sticky)
    3. media query
    4. 오타 수정 외

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 "use strict";
 
-window.addEventListener("scroll", func);
-function func(e) {
-  let header = document.querySelector(".header");
-  if (this.scrollY > 56) {
-    header.style.position = "fixed";
-  } else if (this.scrollY == 0) header.style.position = "relative";
+// 스크롤 시 메뉴 고정
+window.addEventListener("scroll", stickyNavbar);
+function stickyNavbar() {
+  let navbar = document.querySelector(".navbar");
+  if (this.scrollY > 20) {
+    navbar.classList.add("sticky");
+  } else if (this.scrollY == 0) navbar.classList.remove("sticky");
+}
+
+// 메뉴 버튼 클릭 시 확장
+let menuBtn = document.querySelector(".menu-btn");
+menuBtn.addEventListener("click", toggleMenu);
+function toggleMenu(e) {
+  let menu = document.querySelector(".menu");
+  menu.classList.toggle("active");
+  let menuIcon = menuBtn.querySelector("i");
+  menuIcon.classList.toggle("active");
 }
