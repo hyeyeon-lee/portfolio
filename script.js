b922c600c830cb0788d79e62ce982286ebee2a34
hyeyeon-lee <dear.yeonee@gmail.com>
2021-04-17 14:25:06 +0900

    feat: teams html/css 추가
    1. jquery + owl-carousel

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ function toggleMenu(e) {
   let menuIcon = menuBtn.querySelector("i");
   menuIcon.classList.toggle("active");
 }
+
+/* carousel script */
+$(".carousel").owlCarousel({
+  margin: 20,
+  loop: true,
+  autoPlayTimeOut: 2000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 3,
+      nav: false,
+    },
+  },
+});
