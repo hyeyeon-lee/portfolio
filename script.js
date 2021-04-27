52361e19ba3ed4f8905d59f592abeb1f96ffc791
hyeyeon-lee <dear.yeonee@gmail.com>
2021-04-27 22:03:50 +0900

    feat: footer html/css 추가 + scroll top

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ function stickyNavbar() {
   if (this.scrollY > 20) {
     navbar.classList.add("sticky");
   } else if (this.scrollY == 0) navbar.classList.remove("sticky");
+
+  let scrollBtn = document.querySelector(".scroll-up-btn");
+  if (this.scrollY > 500) scrollBtn.classList.add("show");
+  else scrollBtn.classList.remove("show");
+}
+
+// scroll top 버튼
+let scrollBtn = document.querySelector(".scroll-up-btn");
+scrollBtn.addEventListener("click", scrollToTop);
+function scrollToTop() {
+  window.scroll(0, 0);
 }
 
 // 메뉴 버튼 클릭 시 확장
